4cf9ead71ccfded5929cdd3e1a2e71594b3c6e55
Gayatri Soni <54921052+gayatrisoni@users.noreply.github.com>
2023-10-05 05:55:12 +0000

    "Submission for module1 PE01-Gayatri Soni"

---
+++ PE01-HelloWorld/hello-world/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Gayatri Soni</Text>
+      <Text>MSCS</Text>
+      <Text>City University of Seattle</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

33d7af28d0c8fc315bb09b11a2654cd9fa146487
gayatrisoni <54921052+gayatrisoni@users.noreply.github.com>
2023-10-28 07:52:44 +0530

    Copy repo from HOS04 to PE04

---
+++ PE03/todos/TodoList.js
@@ -0,0 +1,25 @@
+// Listing 3.20 Passing toggleComplete and deleteTodo as props to ToDo
+
+import React from 'react'
+import { View } from 'react-native'
+import Todo from './Todo'
+
+const TodoList = ({ todos, deleteTodo, toggleComplete }) => {
+  todos = todos.map((todo, i) => {
+    return (
+      <Todo
+        deleteTodo={deleteTodo}
+        toggleComplete={toggleComplete}
+        key={i}
+        todo={todo} />
+    )
+  })
+
+  return (
+    <View>
+      {todos}
+    </View>
+  )
+}
+
+export default TodoList

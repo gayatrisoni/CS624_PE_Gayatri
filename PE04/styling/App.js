84cb877e0e92159ca317ed370cbe84671f05913d
Gayatri Soni <54921052+gayatrisoni@users.noreply.github.com>
2023-11-16 04:27:18 +0000

    "Submission for PE04 Part2-Gayatri Soni"

--- PE04/styling/App.js
+++ PE04/styling/App.js
@@ -1,22 +1,6 @@
-// Listing 5.3 Scaling ProfileCard from full size to thumbnail
-/*
-    If you press the thumbnail, the component will return to full size. 
-    If you press the full-size component, it will collapse back down into a thumbnail view.
-    By reorganizing the structure of the component, you can better handle adding more ProfileCard components to the application. 
-    
-    In section 5.3, you’ll add more ProfileCards and see how to organize them into a gallery layout.
-*/
 import React, { Component } from 'react';
-// PropTypes lets you specify what properties the ProfileCard component can accept.
 import PropTypes from 'prop-types'; 
-
-// The immutability helper function update lets you update a specific piece of the component’s state.
-// npm install immutability-helper --save
 import update from 'immutability-helper'; 
-
-/*
-  Imports the Platform utility component to programmatically select styles based on the platform.
-*/
 import { Platform, Image, StyleSheet, Text, View, TouchableHighlight } from 'react-native'; 
 
 const userImage = require('./user.png');
@@ -27,7 +11,46 @@ const data = [{
     occupation: 'React Native Developer',
     description: 'John is a really great Javascript developer. ' + 'He loves using JS to build React Native applications ' + 'for iOS and Android',
     showThumbnail: true
-  }
+  },
+  {
+    image: userImage,
+    name: 'John Doe',
+    occupation: 'React Native Developer',
+    description: 'John is a really great Javascript developer. ' + 'He loves using JS to build React Native applications ' + 'for iOS and Android',
+    showThumbnail: true
+  },
+  {
+    image: userImage,
+    name: 'John Doe',
+    occupation: 'React Native Developer',
+    description: 'John is a really great Javascript developer. ' + 'He loves using JS to build React Native applications ' + 'for iOS and Android',
+    showThumbnail: true
+  },
+  {
+    image: userImage,
+    name: 'John Doe',
+    occupation: 'React Native Developer',
+    description: 'John is a really great Javascript developer. ' + 'He loves using JS to build React Native applications ' + 'for iOS and Android',
+    showThumbnail: true
+  },
+  {
+    image: userImage,
+    name: 'John Doe',
+    occupation: 'React Native Developer',
+    description: 'John is a really great Javascript developer. ' + 'He loves using JS to build React Native applications ' + 'for iOS and Android',
+    showThumbnail: true
+  },
+  {
+    image: userImage,
+    name: 'John Doe',
+    occupation: 'React Native Developer',
+    description: 'John is a really great Javascript developer. ' + 'He loves using JS to build React Native applications ' + 'for iOS and Android',
+    showThumbnail: true
+  },
+
+  
+  
+  
 ];
 
 const ProfileCard = (props) => {
@@ -91,18 +114,32 @@ export default class App extends Component {
   render() {
     const list = this.state.data.map(function(item, index) { 
       const { image, name, occupation, description, showThumbnail } = item;
-      return <ProfileCard key={'card-' + index}
-                   image={image}
-                   name={name}
-                   occupation={occupation}
-                   description={description}
-                   onPress={this.handleProfileCardPress.bind(this, index)}
-                   showThumbnail={showThumbnail}/>
+      return(
+        <ProfileCard key={'card-' + index}
+                      image={image}
+                      name={name}
+                      occupation={occupation}
+                      description={description}
+                      onPress={() => this.handleProfileCardPress(index)}
+                      showThumbnail={showThumbnail}/>
+      ); 
     }, this);
+
+    const rows = [];
+    for (let i = 0; i < list.length; i += 2) {
+      const row = (
+        <View key={i/2} style={styles.cardRow}>
+          {list.slice(i, i + 2)}
+        </View>
+      );
+      rows.push(row);
+    }
 
     return (
       <View style={styles.container}>
-        {list} 
+        <View style={[styles.wrapContainer]}>
+          {rows} 
+        </View>
       </View>
     );
   }
@@ -111,110 +148,100 @@ export default class App extends Component {
 const profileCardColor = 'dodgerblue';
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    /*
-      Adds a drop shadow to the card container based on the platform
-    */
-    cardContainer: {
-        alignItems: 'center',
-        borderColor: 'black',
-        borderWidth: 3,
-        borderStyle: 'solid',
-        borderRadius: 20,
-        backgroundColor: profileCardColor,
-        width: 300,
-        height: 400,
-        ...Platform.select({ 
-          ios: {
-            shadowColor: 'black',
-            shadowOffset: {
-              height: 10
-            },
-            shadowOpacity: 1
-          },
-          android: {
-            elevation: 15
-          }
-        })
-    },
-    /*
-      Adds a drop shadow to the circular image container
-    */
-    cardImageContainer: {
-        alignItems: 'center',
-        backgroundColor: 'white',
-        borderWidth: 3,
-        borderColor: 'black',
-        width: 120,
-        height: 120,
-        borderRadius: 60,
-        marginTop: 30,
-        paddingTop: 15,
-        ...Platform.select({ 
-          ios: {
-            shadowColor: 'black',
-            shadowOffset: {
-              height: 10,
-            },
-            shadowOpacity: 1
-          },
-          android: {
-            borderWidth: 3,
-            borderColor: 'black',
-            elevation: 15
-          }
-        })
-    },
-    cardImage: {
-        width: 80,
-        height: 80
-    },
-    /*
-        You can use the textShadowColor, textShadowOffset, and textShadowRadius properties to add a shadow to a Text element. 
-        To create a shadow, you need to specify three things:
-            The color
-            The offset
-            The radius
-
-        The offset specifies the position of the shadow relative to the component casting the shadow. 
-        The radius basically defines how blurry the shadow appears.
-    */
-    cardName: {
-        color: 'white',
-        fontWeight: 'bold',
-        fontSize: 24,
-        marginTop: 30,
-        textShadowColor: 'black',
-        textShadowOffset: {
-            height: 5,
-            width: 5
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  cardContainer: {
+    alignSelf: 'center',
+    alignItems: 'center',
+    borderColor: 'black',
+    borderWidth: 3,
+    borderStyle: 'solid',
+    borderRadius: 20,
+    backgroundColor: profileCardColor,
+    width: 140,
+    marginBottom: 20,
+    height: 200,
+    ...Platform.select({ 
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: {
+          height: 10
         },
-        textShadowRadius: 3 
-    },
-    cardOccupationContainer: {
-        borderColor: 'black',
-        borderBottomWidth: 3
-    },
-    cardOccupation: {
-        fontWeight: 'bold', 
-        marginTop: 10,
-        marginBottom: 10,
-    },
-    cardDescription: {
-        fontStyle: 'italic', 
-        marginTop: 10,
-        marginRight: 40,
-        marginLeft: 40,
-        marginBottom: 10
-    },
-    /*
-        The cardThumbnail style reduces the component’s size by 80%.
-    */
-        cardThumbnail: {
-          transform: [{scale: 0.2}]
+        shadowOpacity: 1
       },
+      android: {
+        elevation: 15
+      }
+    })
+  },
+  cardImageContainer: {
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderWidth: 3,
+    borderColor: 'black',
+    width: 60,
+    height: 60,
+    borderRadius: 60,
+    marginTop: 15,
+    paddingTop: 7,
+    ...Platform.select({ 
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: {
+          height: 10,
+        },
+        shadowOpacity: 1
+      },
+      android: {
+        borderWidth: 3,
+        borderColor: 'black',
+        elevation: 15
+      }
+    })
+  },
+  cardImage: {
+    width: 40,
+    height: 40
+  },
+  cardName: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 12,
+    marginTop: 15,
+    textShadowColor: 'black',
+    textShadowOffset: {
+        height: 5,
+        width: 5
+    },
+    textShadowRadius: 3 
+  },
+  cardOccupationContainer: {
+    borderColor: 'black',
+    borderBottomWidth: 2
+  },
+  cardOccupation: {
+    fontSize: 8,
+    fontWeight: 'bold', 
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  cardDescription: {
+    fontSize: 7,
+    fontStyle: 'italic', 
+    marginTop: 5,
+    marginRight: 15,
+    marginLeft: 15,
+    marginBottom: 10
+  },
+  cardThumbnail: {
+    transform: [{scale: 0.5}]
+  },
+  wrapContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
 });
